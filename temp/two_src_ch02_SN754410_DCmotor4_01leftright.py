6798d37b8b49de6c3433148690c74146f871bea6
hanuri00 <hanuri00@jbnu.ac.kr>
2025-02-01 17:29:23 +0900

    Add files via upload

---
+++ temp/two_src_ch02_SN754410_DCmotor4_01leftright.py
@@ -0,0 +1,69 @@
+#-*- coding: utf-8 -*-
+
+from gpiozero import Robot, Motor
+from time import sleep
+
+rightWheel = Robot(left=(26, 19), right=(20, 21))
+leftWheel = Robot(left=(17, 27), right=(23, 24))
+
+def go(speed):
+    rightWheel.forward(speed=speed)
+    leftWheel.forward(speed=speed)
+
+def back(speed):
+    rightWheel.backward(speed=speed)
+    leftWheel.backward(speed=speed)
+
+def left(speed):
+    rightWheel.forward(speed=speed)
+    leftWheel.forward(speed=0)
+
+def right(speed):
+    rightWheel.forward(speed=0)
+    leftWheel.forward(speed=speed)
+    
+
+def go_left(speed):
+    rightWheel.forward(speed=speed)
+    leftWheel.forward(speed=speed*0.5)
+    
+
+def go_right(speed):
+    rightWheel.forward(speed=speed*0.5)
+    leftWheel.forward(speed=speed)
+
+def stop():
+    leftWheel.stop()
+    rightWheel.stop()
+
+try:
+    while True:
+        go(1)
+        sleep(2)
+        stop()
+        sleep(0.5)
+        back(1)
+        sleep(2)
+        stop()
+        sleep(0.5)
+        left(1)
+        sleep(2)
+        stop()
+        sleep(0.5)
+        right(1)
+        sleep(2)
+        stop()
+        sleep(0.5)
+        go_left(1)
+        sleep(2)
+        stop()
+        sleep(0.5)
+        go_right(1)
+        sleep(2)
+        stop()
+    
+        sleep(0.1)
+
+except KeyboardInterrupt:
+    stop()
+    print("finished!!")
